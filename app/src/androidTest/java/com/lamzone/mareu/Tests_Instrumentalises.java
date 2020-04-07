cd438e1d1d7cbaffab7c3bdaa1c22d8b6187ffdb
maxPallu <32013577+maxPallu@users.noreply.github.com>
2020-04-07 22:42:10 +0200

    Mise à jour projet

--- app/src/androidTest/java/com/lamzone/mareu/Tests_Instrumentalises.java
+++ app/src/androidTest/java/com/lamzone/mareu/Tests_Instrumentalises.java
@@ -1,6 +1,7 @@
 package com.lamzone.mareu;
 
 import android.view.View;
+import android.widget.EditText;
 
 import org.hamcrest.CoreMatchers;
 import org.junit.Before;
@@ -18,12 +19,17 @@ import androidx.test.runner.AndroidJUnit4;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
 import static androidx.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.endsWith;
 import static org.hamcrest.Matchers.notNullValue;
 
 @RunWith(AndroidJUnit4.class)
@@ -56,32 +62,24 @@ public class Tests_Instrumentalises {
     }
 
     @Test
-    public void filterByDate() {
-        onView(ViewMatchers.withId(R.id.add_meeting_button)).perform(click());
-        onView(ViewMatchers.withId(R.id.valider)).perform(click());
-        onView(ViewMatchers.withId(R.id.filtrer)).perform(click());
-        onData(CoreMatchers.anything())
-                .inRoot(RootMatchers.isPlatformPopup())
-                .inAdapterView(CoreMatchers.<View>instanceOf(MenuPopupWindow.MenuDropDownListView.class))
-                .atPosition(0)
-                .perform(click());
-        onData(CoreMatchers.anything())
-                .inRoot(RootMatchers.isPlatformPopup())
-                .inAdapterView(CoreMatchers.<View>instanceOf(MenuPopupWindow.MenuDropDownListView.class))
-                .atPosition(0)
-                .perform(click());
-    }
-
-    @Test
     public void filterByPlace() {
+        //Ajoute une première réunion
         onView(ViewMatchers.withId(R.id.add_meeting_button)).perform(click());
+        onView(ViewMatchers.withId(R.id.meetingPlace)).perform(typeText("Test"));
         onView(ViewMatchers.withId(R.id.valider)).perform(click());
+        //Ajoute une deuxième réunion
+        onView(ViewMatchers.withId(R.id.add_meeting_button)).perform(click());
+        onView(ViewMatchers.withId(R.id.meetingPlace)).perform(typeText("Salle B"));
+        onView(ViewMatchers.withId(R.id.valider)).perform(click());
+        //Filtre les réunions
         onView(ViewMatchers.withId(R.id.filtrer)).perform(click());
         onData(CoreMatchers.anything())
                 .inRoot(RootMatchers.isPlatformPopup())
                 .inAdapterView(CoreMatchers.<View>instanceOf(MenuPopupWindow.MenuDropDownListView.class))
                 .atPosition(1)
                 .perform(click());
+        onView(ViewMatchers.withId(R.id.trier_lieu)).perform(typeText("Te"));
+        onView(ViewMatchers.withId(R.id.Recyclerview)).check(matches(hasMinimumChildCount(1)));
     }
 
 }
--- app/src/main/java/com/lamzone/mareu/view/MeetingAdapter.java
+++ app/src/main/java/com/lamzone/mareu/view/MeetingAdapter.java
@@ -1,5 +1,6 @@
 package com.lamzone.mareu.view;
 
+import android.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
